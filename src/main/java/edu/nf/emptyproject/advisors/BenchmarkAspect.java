ed0596caac740dec130cebc06104eb9f448a6b3f
lornix <lorniu@gmail.com>
2017-02-27 00:23:26 +0800

    ref

--- src/main/java/edu/nf/emptyproject/advisors/BenchmarkAspect.java
+++ src/main/java/edu/nf/emptyproject/advisors/BenchmarkAspect.java
@@ -4,10 +4,13 @@ package edu.nf.emptyproject.advisors;
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
+import org.springframework.core.Ordered;
+import org.springframework.core.annotation.Order;
 import org.springframework.stereotype.Component;
 
 @Component
 @Aspect
+@Order(Ordered.HIGHEST_PRECEDENCE)
 public class BenchmarkAspect {
 
     @Around("execution(* *..*Service.*(..))")
@@ -19,7 +22,7 @@ public class BenchmarkAspect {
         // 结束时间
         long after = System.currentTimeMillis();
         // 输出耗费的时间
-        System.out.printf(">>> 执行方法 [%s] 一共用了 %d ms\n\n", joinPoint.getSignature().getName(), after - before);
+        System.out.printf("\n>>> 执行方法 [%s] 一共用了 %d ms\n\n\n", joinPoint.getSignature().getName(), after - before);
 
         return ret;
     }
